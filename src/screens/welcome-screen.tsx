b0fc8a05efe04721355d226ccd310ab1093c5351
Hasan Diab <admin@quantumpinnaclematrix.tech>
2024-02-02 23:50:20 +0200

    done with home screen navigation style

--- src/screens/welcome-screen.tsx
+++ src/screens/welcome-screen.tsx
@@ -1,10 +1,108 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { ArrowUpRightIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
+import { RootStackParamList } from "../types";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 const WelcomeScreen = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const [fontsLoaded, fontsError] = useFonts({
+    SpaceGroteskSemiBold: require("../../assets/fonts/SpaceGrotesk-SemiBold.ttf"),
+    SpaceGroteskBold: require("../../assets/fonts/SpaceGrotesk-Bold.ttf"),
+    SpaceGroteskMedium: require("../../assets/fonts/SpaceGrotesk-Medium.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontsError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded || fontsError]);
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View
+      onLayout={onLayoutRootView}
+      className="flex-1 "
+      style={{
+        width: wp(100),
+      }}
+    >
+      <View
+        className="justify-center items-center"
+        style={{ width: wp(100), height: hp(100) }}
+      >
+        {/* Heart Icon Image */}
+
+        <View
+          className="justify-center items-center my-4"
+          style={{ width: wp(100) }}
+        >
+          <Image
+            source={require("../../assets/HeartIcon.png")}
+            style={{
+              width: wp(100),
+              height: hp(40),
+              resizeMode: "cover",
+            }}
+          />
+        </View>
+        {/* Welcome Text */}
+        <View className="w-full p-6 px-10">
+          <Text
+            className="tracking-widest leading-none"
+            style={{
+              fontSize: wp(10),
+              fontFamily: "SpaceGroteskBold",
+            }}
+          >
+            Embrace
+          </Text>
+          <Text
+            className="tracking-widest w-[70%] leading-none"
+            style={{
+              fontSize: wp(10),
+              fontFamily: "SpaceGroteskBold",
+            }}
+          >
+            A new Way of Dating
+          </Text>
+          <Text
+            className="text-ray-800 leading-6 tracking-wider w-[70%] mt-2"
+            style={{
+              fontSize: wp(4),
+              fontFamily: "SpaceGroteskMedium",
+            }}
+          >
+            We combine cutting edge technologies with a modern approach to
+            matchmaking.
+          </Text>
+        </View>
+        {/* Button */}
+        <View className="w-full px-10">
+          <TouchableOpacity
+            className="bg-[#F26322] px-4 py-4 rounded-xl flex-row justify-center items-center w-[45%]"
+            onPress={() => navigation.navigate("HomeTabs")}
+          >
+            <Text
+              className="text-white font-bold mr-2"
+              style={{
+                fontSize: wp(3.5),
+                fontFamily: "SpaceGroteskMedium",
+              }}
+            >
+              Get Started
+            </Text>
+            <ArrowUpRightIcon color={"white"} size={20} strokeWidth={2.5} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type RootStackParamList = {
+  Welcome: undefined;
+  ChatDetails: undefined; // Add parameters if any, e.g., { userId: string }
+  HomeTabs: undefined;
+};
+
+export type HomeTabParamList = {
+  Home: undefined;
+  Profile: undefined;
+  Chat: undefined;
+};
