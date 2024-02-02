778cc9a835fb264ca68d666cd35bf215ae331e7f
Hasan Diab <admin@quantumpinnaclematrix.tech>
2024-02-02 21:15:56 +0200

    trying tailwind css and adding a screens

---
+++ src/screens/chat/chat-details-screen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CHatDetailsScreen = () => {
+  return (
+    <View>
+      <Text>CHatDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default CHatDetailsScreen;
